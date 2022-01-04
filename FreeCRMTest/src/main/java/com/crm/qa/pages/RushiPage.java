06c330cfeeb2a698395b71709af1e8b39062b7d3
rvkathar <rvkathar5@gmail.com>
2022-01-04 09:18:50 +0530

    new rushi classes added from rushi branch

---
+++ FreeCRMTest/src/main/java/com/crm/qa/pages/RushiPage.java
@@ -0,0 +1,9 @@
+package com.crm.qa.pages;
+
+import com.crm.qa.base.TestBase;
+
+public class RushiPage extends TestBase {
+public static void main(String[] args) {
+	System.out.println("this is rushi page");
+}
+}
---
+++ FreeCRMTest/src/test/java/com/crm/qa/testcases/RushiPageTest.java
@@ -0,0 +1,9 @@
+package com.crm.qa.testcases;
+
+import com.crm.qa.base.TestBase;
+
+public class RushiPageTest extends TestBase {
+public static void main(String[] args) {
+	System.out.println("this is rushi test page");
+}
+}
